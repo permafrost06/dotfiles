c70c80be7a6b2e71717040311da95e9c7ceb6d3e
Zahin Azmayeen <zahin06@gmail.com>
2024-05-21 11:38:25 +0600

    telescope: skip previewing large files

--- .config/nvim/lua/permafrost/plugins/telescope.lua
+++ .config/nvim/lua/permafrost/plugins/telescope.lua
@@ -11,7 +11,28 @@ return {
         },
     },
     config = function()
+        local previewers = require('telescope.previewers')
+
+        local new_maker = function(filepath, bufnr, opts)
+            opts = opts or {}
+
+            filepath = vim.fn.expand(filepath)
+            vim.loop.fs_stat(filepath, function(_, stat)
+                if not stat then return end
+                if stat.size > 100000 then
+                    return
+                else
+                    previewers.buffer_previewer_maker(filepath, bufnr, opts)
+                end
+            end)
+        end
+
+        require('telescope').setup {
+            defaults = {
+                buffer_previewer_maker = new_maker,
+            }
+        }
+
         require("telescope").load_extension("live_grep_args")
     end
 }
-

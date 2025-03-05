47bd6826b986709b6c8cc6e467239cf83e23c42a
Zahin Azmayeen <zahin06@gmail.com>
2025-03-05 16:39:11 +0600

    remove kanagawa and add gruvbox theme with light-dark switch

--- .config/nvim/lua/permafrost/plugins/light-dark.lua
+++ .config/nvim/lua/permafrost/plugins/light-dark.lua
@@ -3,10 +3,12 @@ return {
     opts = {
         update_interval = 1000,
         set_dark_mode = function()
-            vim.cmd.colorscheme("kanagawa-dragon")
+            vim.o.background = "dark"
+            vim.cmd.colorscheme('gruvbox-material')
         end,
         set_light_mode = function()
-            vim.cmd.colorscheme("kanagawa-lotus")
+            vim.o.background = "light"
+            vim.cmd.colorscheme('gruvbox-material')
         end,
     },
 }

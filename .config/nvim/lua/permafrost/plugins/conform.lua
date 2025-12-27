cdf3b492bf31770f8b5cf5d764fccafa0ab6e5fa
Zahin Azmayeen <zahin06@gmail.com>
2025-12-27 13:08:44 +0600

    nvim add format on save

---
+++ .config/nvim/lua/permafrost/plugins/conform.lua
@@ -0,0 +1,21 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	opts = {
+		formatters_by_ft = {
+			javascript = { "prettier" },
+			javascriptreact = { "prettier" },
+			typescript = { "prettier" },
+			typescriptreact = { "prettier" },
+			css = { "prettier" },
+			scss = { "prettier" },
+			json = { "prettier" },
+		},
+		format_on_save = {
+			-- These options will be passed to conform.format()
+			timeout_ms = 500,
+			lsp_fallback = true,
+		},
+	},
+}

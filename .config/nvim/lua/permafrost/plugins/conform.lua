41479e23d494d6307da389917c07e90c1c46d602
Zahin Azmayeen <zahin06@gmail.com>
2026-02-23 16:25:05 +0600

    update conform config for php

--- .config/nvim/lua/permafrost/plugins/conform.lua
+++ .config/nvim/lua/permafrost/plugins/conform.lua
@@ -1,21 +1,37 @@
 return {
-	"stevearc/conform.nvim",
-	event = { "BufWritePre" },
-	cmd = { "ConformInfo" },
-	opts = {
-		formatters_by_ft = {
-			javascript = { "prettier" },
-			javascriptreact = { "prettier" },
-			typescript = { "prettier" },
-			typescriptreact = { "prettier" },
-			css = { "prettier" },
-			scss = { "prettier" },
-			json = { "prettier" },
-		},
-		format_on_save = {
-			-- These options will be passed to conform.format()
-			timeout_ms = 500,
-			lsp_fallback = true,
-		},
-	},
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    config = function()
+        local conform = require("conform")
+        local util = require("conform.util")
+
+        conform.setup({
+            formatters_by_ft = {
+                javascript = { "prettier" },
+                javascriptreact = { "prettier" },
+                typescript = { "prettier" },
+                typescriptreact = { "prettier" },
+                css = { "prettier" },
+                scss = { "prettier" },
+                json = { "prettier" },
+                php = { "php_cs_fixer" },
+            },
+            formatters = {
+                php_cs_fixer = {
+                    cwd = util.root_file({
+                        ".php-cs-fixer.php",
+                        ".php-cs-fixer.dist.php",
+                        "composer.json",
+                    }),
+                },
+            },
+            format_on_save = function(bufnr)
+                return {
+                    timeout_ms = 3000,
+                    lsp_fallback = vim.bo[bufnr].filetype ~= "php",
+                }
+            end,
+        })
+    end,
 }
